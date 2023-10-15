0783edbe111a7b32fbbc30248186fa42ceffd56c
roben <asakatol1234@gmail.com>
2023-10-15 21:57:27 +0800

    add features.jsx, about.jsx, css and photo

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,78 +1,76 @@
-import React from 'react'
-import {useState} from 'react';
+import React from "react";
+import { useState } from "react";
 import { MDBIcon } from "mdb-react-ui-kit";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Dropdown,
-    DropdownToggle,
-    DropdownItem,
-    DropdownMenu,
-    UncontrolledDropdown
-  } from 'reactstrap';
-import '../../css/header.css'
-import omWhiteLogo from '../../assets/omWhiteLogo.png'
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Dropdown,
+  DropdownToggle,
+  DropdownItem,
+  DropdownMenu,
+  UncontrolledDropdown,
+} from "reactstrap";
+import "../../css/header.css";
+import omWhiteLogo from "../../assets/omWhiteLogo.png";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen)
+  const [deviceWidth, _] = useState(0);
 
-    const [deviceWidth, _] = useState(0)
-    
-    return(
-      <>
+  return (
+    <>
+      {/* <div className='navbar-container'> */}
+      <Navbar expand={deviceWidth} className="navbar-container">
+        <a href="/">
+          <div className="nav-logo2"></div>
+          <img src={omWhiteLogo} alt="OrganizeMe-Logo" id="nav-logo" />
+        </a>
 
-        {/* <div className='navbar-container'> */}
-          <Navbar expand={deviceWidth} className='navbar-container'>
-            <a href="/">
-              <div className='nav-logo2'></div>
-              <img src={omWhiteLogo} alt="OrganizeMe-Logo" id='nav-logo'/>
-            </a>
+        <div className="nav-links">
+          <NavLink href="/features" id="feat">
+            Features
+          </NavLink>
+          <NavLink href="/about" id="about">
+            About
+          </NavLink>
+          <NavLink href="" id="login">
+            Login
+          </NavLink>
+        </div>
 
-            <div className='nav-links'>
-              <NavLink href='' id='feat'>
-                Features
-              </NavLink>
-              <NavLink href="/about" id='about'>
-                About
-              </NavLink>
-              <NavLink href='' id='login'>
-                Login
-              </NavLink>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar className="nav-dropdown">
+          <Nav className="burger-menu" navbar>
+            <div className="burger-menu-container">
+              <NavItem>
+                <NavLink href="" id="feat">
+                  Features
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/about" id="about">
+                  About
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="" id="login">
+                  Login
+                </NavLink>
+              </NavItem>
             </div>
-
-            <NavbarToggler onClick={toggle}/>
-            <Collapse isOpen={isOpen} navbar className='nav-dropdown'>
-              <Nav className="burger-menu" navbar>
-                <div className='burger-menu-container'>
-                  <NavItem>
-                    <NavLink href='' id='feat'>
-                      Features
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href='/about' id='about'>
-                      About
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href='' id='login'>
-                      Login
-                    </NavLink>
-                  </NavItem>
-                </div>
-              </Nav>
-            </Collapse>
-          </Navbar>
-        {/* </div> */}
-      </>
-    )
-}
+          </Nav>
+        </Collapse>
+      </Navbar>
+      {/* </div> */}
+    </>
+  );
+};
 
 export default Header;
